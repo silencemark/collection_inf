bd67bdc0612a487e32d2289f95a09fb80622ee94
silence <lucky_markli@sina.com>
2020-06-01 00:48:50 +0800

    登录以及首页功能接口

---
+++ src/main/java/com/collection/service/IAppIndexService.java
@@ -0,0 +1,37 @@
+package com.collection.service;
+
+import java.util.List;
+import java.util.Map;
+/**
+ * app首页相关
+ * @author silence
+ *
+ */
+public interface IAppIndexService {
+	
+	/**
+	 * 获取首页banner图
+	 * @param data
+	 * @return
+	 * @author silence
+	 */
+	List<Map<String, Object>> getHomePageBanner(Map<String, Object> data);
+	
+	/**
+	 * 获取首页广告信息
+	 * @param data
+	 * @return
+	 * @author silence
+	 */
+	List<Map<String, Object>> getAdvertisement(Map<String, Object> data);
+	
+	/**
+	 * 获取首页免费影片列表信息
+	 * @param data
+	 * @return
+	 * @author silence
+	 */
+	List<Map<String, Object>> getHomePageMovie(Map<String, Object> data);
+	
+	
+}
---
+++ src/main/java/com/collection/service/IAppLoginService.java
@@ -0,0 +1,53 @@
+package com.collection.service;
+
+import java.util.Map;
+/**
+ * app登录相关
+ * @author silence
+ *
+ */
+public interface IAppLoginService {
+	/**
+	 * 登录
+	 * @param data
+	 * @return
+	 * @author silence
+	 */
+	Map<String, Object> login(Map<String, Object> data);
+	
+	/**
+	 * @param data
+	 * @return
+	 * @author silence
+	 * 检验手机号
+	 */
+	boolean checkPhone(Map<String, Object> data);
+	
+	
+	/**
+	 * @param data
+	 * @return
+	 * @author silence
+	 * 新增用户
+	 * @return 
+	 */
+	void insertUserInfo(Map<String, Object> data);
+	
+	/**
+	 * @param data
+	 * @return
+	 * @author silence
+	 * 修改密码
+	 * @return 
+	 */
+	void updateUserInfo(Map<String, Object> data);
+	
+	/**
+	 * @param data
+	 * @return
+	 * @author silence
+	 * 获取用户信息
+	 * @return data
+	 */
+	Map<String, Object> getUserInfo(Map<String, Object> data);
+}
